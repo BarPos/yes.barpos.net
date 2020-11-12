79e39c4f9b5bfe8c37a3d3808b014f3e0ea1cf14
BarPosWasTaken <ketsopb@gmail.com>
2020-11-12 21:43:56 +0100

    epic

--- js.js
+++ js.js
@@ -6,11 +6,15 @@ const p3 = document.getElementById('p3')
 p2.style.display = 'none'
 p3.style.display = 'none'
 
+var bg = new Audio('bg.mp3');
+bg.play();
+
 function yes(){
     body.style.animation = 'scroll 120s linear infinite'
     p1.style.display = 'none'
     p2.style.display = ''
     var audio = new Audio('halo.mp3');
+    bg.pause()
     audio.play();
 }
 function no(){
@@ -18,5 +22,6 @@ function no(){
     p3.style.display = ''
     body.style.animation = 'scroll 4s linear infinite'
     var audio = new Audio('errape.mp3');
+    bg.pause()
     audio.play();
 }
